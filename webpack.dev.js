c9c5448a85d87cff0aeb62b0e3615bf7aba0b8f9
mbehboodi <mbehboodi6909@gmail.com>
2019-01-08 11:31:54 +0330

    add webpack configuration

---
+++ webpack.dev.js
@@ -0,0 +1,125 @@
+'use strict';
+
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const webpack = require('webpack');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const WriteFilePlugin = require('write-file-webpack-plugin');
+
+const TemplateMeta = {
+  viewport: {
+    viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+  },
+  charset: {
+    'charset': 'UTF-8'
+  }
+};
+
+module.exports = merge(common,
+  {
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?/,
+          loader: 'ts-loader',
+          include: [
+            path.resolve(__dirname, 'src')
+          ]
+        },
+        {
+          test: /\.scss$/,
+          include: [
+            path.resolve(__dirname, 'src')
+          ],
+          use: [
+            {
+              loader: 'style-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                camelCase: true,
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'resolve-url-loader',
+              options: {
+                sourceMap: true,
+                keepQuery: true,
+                debug: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceComments: true,
+                sourceMap: true,
+                sourceMapContents: true,
+                outputStyle: 'expanded',
+                includePaths: [
+                  './node_modules'
+                ]
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            publicPath: '../assets/fonts',
+            outputPath: 'assets/fonts'
+          }
+        },
+        {
+          test: /\.pug$/,
+          loader: 'pug-loader'
+        },
+        {
+          test: /\.html$/,
+          include: [
+            path.join(__dirname, 'src', 'components', 'Visualizer')
+          ],
+          use: {
+            loader: 'html-loader'
+          }
+        }
+      ]
+    },
+    devtool: 'source-map',
+    plugins: [
+      new CleanWebpackPlugin([path.resolve(__dirname, 'dist')]),
+      new webpack.HashedModuleIdsPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src', 'index.pug'),
+        filename: path.join('index.html'),
+        chunks: ['main'],
+        meta: TemplateMeta
+      }),
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false
+      }),
+      new WriteFilePlugin()
+    ],
+    devServer: {
+      contentBase: './dist',
+      port: 8080,
+      open: true
+    }
+  }
+);
