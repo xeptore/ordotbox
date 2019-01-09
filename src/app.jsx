29f727f11727741c6e980e1e5740fffd88cfc5f5
mbehboodi <mbehboodi6909@gmail.com>
2019-01-09 08:56:08 +0330

    add edges click event handler

--- src/app.jsx
+++ src/app.jsx
@@ -8,10 +8,18 @@ import { times } from 'lodash';
 const n = 3;
 const uids = {
     row: 0,
-    cell: 0
+    cell: -1
 };
 
 class App extends React.Component {
+    handleClick(e) {
+        if (e.target.getAttribute('clicked')) {
+            return;
+        }
+        e.target.parentNode.classList.add('clicked');
+        e.target.classList.add('clicked');
+        e.target.setAttribute('clicked', true);
+    }
     render() {
         return (
             <div className="wrapper">
@@ -21,22 +29,23 @@ class App extends React.Component {
                             <div className="row" key={uids.row++}>
                                 {
                                     times(2 * n + 1, j => {
+                                        uids.cell++;
                                         if (i % 2 === 0 && j % 2 === 0) {
-                                            return <div className="cell vertex" key={uids.cell++}></div>;
-                                        } else if (i % 2 === 0 && j % 2 === 1) {
-                                            return (
-                                                <div className="cell edge vedge" key={uids.cell++}>
-                                                    <a className="hoverable"></a>
-                                                </div>
-                                            );
+                                            return <div className="cell vertex" key={uids.cell} data-uid={uids.cell}></div>;
                                         } else if (i % 2 === 1 && j % 2 === 0) {
                                             return (
-                                                <div className="cell edge hedge" key={uids.cell++}>
-                                                    <a className="hoverable"></a>
+                                                <div className="cell edge vedge" key={uids.cell} data-uid={uids.cell}>
+                                                    <a className="hoverable" onClick={this.handleClick}></a>
+                                                </div>
+                                            );
+                                        } else if (i % 2 === 0 && j % 2 === 1) {
+                                            return (
+                                                <div className="cell edge hedge" key={uids.cell} data-uid={uids.cell}>
+                                                    <a className="hoverable" onClick={this.handleClick}></a>
                                                 </div>
                                             );
                                         } else {
-                                            return <div className="cell box" key={uids.cell++}></div>;
+                                            return <div className="cell box" key={uids.cell} data-uid={uids.cell}></div>;
                                         }
                                     })
                                 }
