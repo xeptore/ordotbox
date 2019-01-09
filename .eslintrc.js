483c933b370fc9581f980d81b93210f536754b92
mbehboodi <mbehboodi6909@gmail.com>
2019-01-09 08:22:26 +0330

    add eslint react plugin for un-used variable error on react components

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
         "es6": true,
         "node": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
