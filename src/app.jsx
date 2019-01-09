e73b005cbcdc86aa4522c73b3ed7badbb1e1e960
mbehboodi <mbehboodi6909@gmail.com>
2019-01-09 08:17:51 +0330

    changing structure from table to divs of divs

--- src/app.jsx
+++ src/app.jsx
@@ -8,49 +8,43 @@ import { times } from 'lodash';
 const n = 3;
 const uids = {
     row: 0,
-    col: 0
+    cell: 0
 };
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.rows = [];
-        for (let i = 0; i < 2 * n + 1; i++) {
-            this.rows.push(i % 2 === 0 ? <tr key={uids.row}>{this.evencols()}</tr> : <tr key={uids.row}>{this.oddcols()}</tr>);
-            uids.row++;
-        }
-    }
-    evencols() {
-        const tds = [];
-        times(2 * n + 1, i => {
-            let td = <td key={uids.col} className='unselectable'></td>;
-            if (i % 2 === 1) {
-                td = <td key={uids.col} className='row selectable'><a></a></td>;
-            }
-            tds.push(td);
-            uids.col++;
-        });
-        return tds;
-    }
-    oddcols() {
-        const tds = [];
-        times(2 * n + 1, i => {
-            let td = <td key={uids.col} className='box'><a></a></td>;
-            if (i % 2 === 0) {
-                td = <td key={uids.col} className='col selectable'><a></a></td>;
-            }
-            tds.push(td);
-            uids.col++;
-        });
-        return tds;
-    }
     render() {
         return (
-            <table>
-                <tbody>
-                    {this.rows}
-                </tbody>
-            </table>
+            <div className="wrapper">
+                {
+                    times(2 * n + 1, i => {
+                        return (
+                            <div className="row" key={uids.row++}>
+                                {
+                                    times(2 * n + 1, j => {
+                                        if (i % 2 === 0 && j % 2 === 0) {
+                                            return <div className="cell vertex" key={uids.cell++}></div>;
+                                        } else if (i % 2 === 0 && j % 2 === 1) {
+                                            return (
+                                                <div className="cell edge vedge" key={uids.cell++}>
+                                                    <a className="hoverable"></a>
+                                                </div>
+                                            );
+                                        } else if (i % 2 === 1 && j % 2 === 0) {
+                                            return (
+                                                <div className="cell edge hedge" key={uids.cell++}>
+                                                    <a className="hoverable"></a>
+                                                </div>
+                                            );
+                                        } else {
+                                            return <div className="cell box" key={uids.cell++}></div>;
+                                        }
+                                    })
+                                }
+                            </div>
+                        );
+                    })
+                }
+            </div>
         );
     }
 }
