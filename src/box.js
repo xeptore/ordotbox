2c0186a4e7ecadcc4874f4c1a83ac14f29282ec2
mbehboodi <mbehboodi6909@gmail.com>
2019-01-11 07:45:49 +0330

    add RemaningEdges property to Box class

--- src/box.js
+++ src/box.js
@@ -1,9 +1,11 @@
 import { times } from 'lodash';
 
 export default class Box {
-    constructor(id, edgesID) {
+    constructor(id, element, edgesID) {
         this.ID = id;
         this.Edges = times(4, i => edgesID[i]);
+        this.Element = element;
         this.SelectedEdges = [];
+        this.RemainingEdges = 4;
     }
 }
